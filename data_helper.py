fcd76f92a2ce845699a2777228e5d2dedd4d76e1
Mrinal Jain <mrinaljain007@gmail.com>
2020-04-24 15:06:32 -0400

    Adding code for denoising autoencoder

--- data_helper.py
+++ data_helper.py
@@ -163,33 +163,23 @@ class LabeledDataset(torch.utils.data.Dataset):
             return image_tensor, target, road_image
 
 
-# The dataset class for unlabeled data.
-class CustomUnlabeledDataset(torch.utils.data.Dataset):
-    def __init__(self, image_folder, scene_index):
+# -------------------------------------------------------------- #
+# The dataset class for unlabeled data for denoising autoencoder #
+# -------------------------------------------------------------- #
+class CorruptedUnlabeledDataset(torch.utils.data.Dataset):
+    def __init__(self, image_folder, scene_index, transform, noise=None):
         """
         Args:
             image_folder (string): the location of the image folder
             scene_index (list): a list of scene indices for the unlabeled data
+            transform (Transform): The function to process the image
         """
 
         self.image_folder = image_folder
         self.scene_index = scene_index
+        self.transform = transform
+        self.noise = noise
         self.first_dim = "image"
-        self.permutations = self.__retrive_permutations(1000)
-        self.__image_transformer = torchvision.transforms.Compose(
-            [
-                torchvision.transforms.Resize(256, Image.BILINEAR),
-                torchvision.transforms.CenterCrop(255),
-            ]
-        )
-        self.__augment_tile = torchvision.transforms.Compose(
-            [
-                torchvision.transforms.RandomCrop(64),
-                torchvision.transforms.Resize((75, 75), Image.BILINEAR),
-                torchvision.transforms.Lambda(rgb_jittering),
-                torchvision.transforms.ToTensor(),
-            ]
-        )
 
     def __len__(self):
         return self.scene_index.size * NUM_SAMPLE_PER_SCENE * NUM_IMAGE_PER_SAMPLE
@@ -207,52 +197,24 @@ class CustomUnlabeledDataset(torch.utils.data.Dataset):
             self.image_folder, f"scene_{scene_id}", f"sample_{sample_id}", image_name,
         )
 
-        img = Image.open(image_path).convert("RGB")
-        if np.random.rand() < 0.30:
-            img = img.convert("LA").convert("RGB")
+        image = Image.open(image_path)
 
-        if img.size[0] != 255:
-            img = self.__image_transformer(img)
+        target_ = self.transform(image)
+        if self.noise is not None:
+            input_ = self.noise(target_)
+        else:
+            input_ = target_.clone()
 
-        s = float(img.size[0]) / 3
-        a = s / 2
-        tiles = [None] * 9
-        for n in range(9):
-            i = n / 3
-            j = n % 3
-            c = [a * i * 2 + a, a * j * 2 + a]
-            c = np.array([c[1] - a, c[0] - a, c[1] + a + 1, c[0] + a + 1]).astype(int)
-            tile = img.crop(c.tolist())
-            tile = self.__augment_tile(tile)
-            # Normalize the patches indipendently to avoid low level features shortcut
-            m, s = (
-                tile.view(3, -1).mean(dim=1).numpy(),
-                tile.view(3, -1).std(dim=1).numpy(),
-            )
-            s[s == 0] = 1
-            norm = torchvision.transforms.Normalize(mean=m.tolist(), std=s.tolist())
-            tile = norm(tile)
-            tiles[n] = tile
-
-        order = np.random.randint(len(self.permutations))
-        data = [tiles[self.permutations[order][t]] for t in range(9)]
-        data = torch.stack(data, 0)
-
-        return data, int(order), tiles
-
-    def __retrive_permutations(self, classes):
-        all_perm = np.load("permutations_1000.npy")
-        # from range [1,9] to [0,8]
-        if all_perm.min() == 1:
-            all_perm = all_perm - 1
-
-        return all_perm
+        return input_, target_
 
 
-def rgb_jittering(im):
-    im = np.array(im, "int32")
-    for ch in range(3):
-        im[:, :, ch] += np.random.randint(-2, 2)
-    im[im > 255] = 255
-    im[im < 0] = 0
-    return im.astype("uint8")
+class AddGaussianNoise(object):
+    def __init__(self, mean=0.0, std=1.0):
+        self.std = std
+        self.mean = mean
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + f"(mean={self.mean}, std={self.std})"
